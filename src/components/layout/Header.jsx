2de4e1b67b812a67bf541b7c42ee81bff1bfe078
naman-shukla-ksolves <naman.shukla@ksolves.com>
2024-01-31 17:09:49 +0530

    changes in header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="container">
       <div className="d-flex flex-column align-items-center flex-md-row p-3 px-md-4 bg-white border-bottom box-shadow">
-        Chat GPT OPEN API
+        Video Script Generator
       </div>
     </header>
   );
